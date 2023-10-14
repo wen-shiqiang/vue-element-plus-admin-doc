7f2e6f4219a32f9bb82ff2b3e9fc4b1f63633e0c
kailong321200875 <kailong321200875@users.noreply.github.com>
2023-10-14 03:25:40 +0000

    deploy: 9b9f99f2d61ed0e6115625947468293babc42009

---
+++ assets/dep_dark.md.d8c68cdb.lean.js
@@ -0,0 +1,1 @@
+import{o as e,c as a,a as r}from"./app.4a0e9503.js";const d='{"title":"黑暗主题","description":"","frontmatter":{},"headers":[{"level":2,"title":"介绍","slug":"介绍"},{"level":2,"title":"切换主题","slug":"切换主题"}],"relativePath":"dep/dark.md","lastUpdated":1697253865808}',t={},h=r('',6);t.render=function(r,d,t,o,i,l){return e(),a("div",null,[h])};export default t;export{d as __pageData};
